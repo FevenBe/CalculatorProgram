a1ae801a6faf764364ab012172618631efe36968
FevenBe <pomimango@gmail.com>
2021-04-15 23:07:29 -0700

    Trial 01

---
+++ BasicCalculator/src/com/basic1/BasicOperation.java
@@ -0,0 +1,17 @@
+package com.basic1;
+
+public class BasicOperation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String name="Pomi mango";
+		int age=15;
+		String city="Addis Ababa";
+				System.out.println("Her name is " + name);
+				System.out.println("she is "+ age +" year old");
+				System.out.println("She now lives in "+ city);
+		
+	}
+
+}
